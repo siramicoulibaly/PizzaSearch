e49c5e4239ad1e6cbb1acd10a71c5c77731fa175
dombacoulibaly <dombacoulibaly@claremontihs.org>
2015-12-16 22:58:31 +0000

    i try to finish to the pizza search today

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$( "# submit" ).click(function() {
-  // do some stuff
+$( document ).ready(
+    $.get("https://api.foursquare.com/v2/venues/search?" +
+        "client_id=RRTEPALM5TOOSR3KMR4VIINY3J5R1UEO1KNDR0L4GQLVFM3M" +
+        "&client_secret=ONWFLV1CZD5I4XSCII202VQKEFWQPGFGYFRDVZ2NYBED4QL4" +
+        "&v=20151209" +
+        "&near=nyc" +
+        "&query=pizza",
+        function(data) {
+            console.log(data)
+        }));
+        
+
+$( "#submit" ).click(function() {
+
 });
+
